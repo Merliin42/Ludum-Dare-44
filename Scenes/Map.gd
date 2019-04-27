33fdb3f98ff5b9c7254b1fdde096ed45653246ce
Shae1507 <lou.bouret@hotmail.fr>
2019-04-27 15:18:55 +0200

    change map

--- Scenes/Map.gd
+++ Scenes/Map.gd
@@ -21,5 +21,4 @@ func mobStart():
 	# add some randomness to the direction
 	direction += rand_range(-PI/4, PI/4)
 	mob.rotation = direction
-#	mob.set_linear_velocity(Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction))
 	mob.move_and_slide(Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction))
